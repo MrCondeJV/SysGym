669d2311006eae18347c2a8c92159a4fe9b76cad
MrCondeJV <lbarriosmunoz1012@gmail.com>
2025-05-16 10:32:23 -0500

    Corregido inicio de sesion

--- App/controllers/login/ingreso.php
+++ App/controllers/login/ingreso.php
@@ -1,5 +1,6 @@
 <?php
 
+
 include('../../config.php');
 session_start();
 
@@ -13,17 +14,12 @@ $query->bindParam(':nombre_usuario', $nombre_usuario, PDO::PARAM_STR);
 $query->execute();
 $usuario = $query->fetch(PDO::FETCH_ASSOC);
 
-
-
 if ($usuario) {
     if (strtolower($usuario['estado']) !== 'activo') {
-        file_put_contents($logFile, "[" . date('Y-m-d H:i:s') . "] Usuario inactivo: $nombre_usuario\n", FILE_APPEND);
         $_SESSION['mensaje'] = "Acceso denegado: Usuario inactivo.";
         header('Location: ../../views/login/index.php');
         exit;
     }
-
-    
 
     if (password_verify($password_user, $usuario['contrasena_hash'])) {
         $_SESSION['sesion_usuario'] = $usuario['nombre_usuario'];
@@ -37,17 +33,11 @@ if ($usuario) {
         $stmt_update = $pdo->prepare($sql_update);
         $stmt_update->execute([$usuario['id_usuario']]);
 
-       
-
         // Redirigir al inicio general
-       header('Location: ../../../index.php ');
+        header('Location: ../../../index.php');
         exit;
-    } else {
-        file_put_contents($logFile, "[" . date('Y-m-d H:i:s') . "] password_verify falló para: $nombre_usuario\n", FILE_APPEND);
     }
-} else {
-    file_put_contents($logFile, "[" . date('Y-m-d H:i:s') . "] Usuario no encontrado: $nombre_usuario\n", FILE_APPEND);
-}
+} 
 
 $_SESSION['mensaje'] = "Error: Datos incorrectos";
 header('Location: ../../views/login/index.php');
